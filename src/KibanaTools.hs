f41444f47746ff9a72d9406f7b39357788f29e7a
Lawrence Jones <lawrjone@gmail.com>
2017-10-16 10:17:39 +0100

    Small cleanup

--- src/KibanaTools.hs
+++ src/KibanaTools.hs
@@ -4,9 +4,7 @@ import System.Environment ()
 import System.IO (stderr, hPutStrLn)
 import Options
 import Data.Maybe
-import Control.Monad
 import Control.Monad.Loops
-import Debug.Trace
 import qualified Data.Vector as Vector (toList)
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as B
@@ -39,6 +37,7 @@ main = runCommand $ \opts args -> do
   runMain opts (head args)
   return ()
 
+runMain :: MainOptions -> String -> IO ()
 runMain opts "query" = do
     queryContents <- B.readFile (optQueryFile opts)
     let query = fromJust.decode $ queryContents :: KibanaQuery
@@ -48,11 +47,9 @@ runMain opts "query" = do
 queryKibanaCommand :: KibanaHost -> Int -> KibanaQuery -> IO [Value]
 queryKibanaCommand host perPage query = do
     page    <- postWith opts url query
-    results <- unfoldM (getResultsM (scroller host (getId page)))
+    results <- unfoldWhileM (not . null) (fmap getResults (scroller host (getId page)))
 
-    return $ case getResults page of
-      Just rs -> concat (rs : results)
-      Nothing -> []
+    return $ concat (getResults page : results)
 
     where
       url     = host ++ "/_search"
@@ -60,15 +57,8 @@ queryKibanaCommand host perPage query = do
       getId   = (^.. responseBody . key "_scroll_id")
       getHits = (^.. responseBody . key "hits" . key "hits")
 
-      getResultsM :: IO (Response B.ByteString) -> IO (Maybe [Value])
-      getResultsM = fmap getResults
-
-      getResults :: Response B.ByteString -> Maybe [Value]
-      getResults page =
-        case unwrapHits (getHits page) of
-          [] -> Nothing
-          hs -> Just hs
-
+      getResults :: Response B.ByteString -> [Value]
+      getResults = unwrapHits . getHits
       unwrapHits hs = case hs of [Array hs'] -> Vector.toList hs'
 
 scroller :: KibanaHost -> [Value] -> IO (Response B.ByteString)
